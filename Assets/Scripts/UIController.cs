f1655bf08c41684114f8da471e52fd6574a8fd5b
Tim Krause <tim.krause@stads.de>
2025-10-07 00:42:17 +1100

    UI controls implementation

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.AI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Image settingsPopup;
+    [SerializeField] AudioSource music;
+    [SerializeField] Slider mouseSensitivitySlider;
+    [SerializeField] Slider yawPitchSlider;
+    [SerializeField] Slider volumeSlider;
+    public TMP_Text levelLabel;
+    public TMP_Text currentKeyText;
+
+    [SerializeField] private Image volumeImage;
+    [SerializeField] private Sprite volumeOnSprite;
+    [SerializeField] private Sprite volumeOffSprite;
+
+    NavMeshAgent agent;
+
+    GameObject player;
+    GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // don't display the popup on start
+        settingsPopup.gameObject.SetActive(false);
+
+        // get references to the player and camera
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        volumeSlider.value = AudioListener.volume;
+        mouseSensitivitySlider.value = player.GetComponent<MouseLook>().sensitivityHor;
+
+        // set the slider value to the player's speed
+        //agent = player.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // display the cursor when the ESC key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // unlock and display the cursor
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void OnCloseSettings()
+    {
+        // don't display the settings popup
+        settingsPopup.gameObject.SetActive(false);
+
+        player.GetComponent<FPSInput>().enabled = true;
+        player.GetComponent<MouseLook>().enabled = true;
+        mainCamera.GetComponent<MouseLook>().enabled = true;
+    }
+
+    public void OnOpenSettings()
+    {
+        // display the settings popup
+        settingsPopup.gameObject.SetActive(true);
+
+        player.GetComponent<FPSInput>().enabled = false;
+        player.GetComponent<MouseLook>().enabled = false;
+        mainCamera.GetComponent<MouseLook>().enabled = false;
+    }
+
+    public void OnMouseSensitivityChange()
+    {
+        // change the mouse sensitivity
+        player.GetComponent<MouseLook>().sensitivityHor = mouseSensitivitySlider.value;
+        player.GetComponent<MouseLook>().sensitivityVert = mouseSensitivitySlider.value;
+        mainCamera.GetComponent<MouseLook>().sensitivityHor = mouseSensitivitySlider.value;
+        mainCamera.GetComponent<MouseLook>().sensitivityVert = mouseSensitivitySlider.value;
+    }
+    public void OnYawPitchChange()
+    {
+        // change the yaw/pitch setting
+        //agent.speed = speedSlider.value;
+    }
+    public void OnVolumeChange()
+    {
+        // change the volume
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    public void Mute()
+    {
+        bool isMute = AudioListener.pause;
+        volumeImage.sprite = isMute ? volumeOffSprite : volumeOnSprite;
+        AudioListener.pause = !isMute;
+    }
+}
